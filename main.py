a04ab616314d667c45ab5269b5be7994dc210761
UrielArias <urielarias.a12@gmail.com>
2025-05-01 03:21:40 -0300

    feat: adding calls to generate ouput data for linear and non-linear perceptrons (needs testing and modularization)

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import pandas as pd
 from typing import List
 from neural_network.models.basic_perceptron import Perceptron
 from stats import plots_for_exercise_1
-from neural_network.activation_functions import step
+from neural_network.activation_functions import step, identity, prime_identity, tanh, prime_tanh, logistic, prime_logistic
 from neural_network.error_functions import squared_error
+from partition_methods import k_cross_validation
+from metric_functions import get_prediction_error
 
 if __name__ == '__main__':
     results_data_dir_name = "output_data"
@@ -48,3 +50,130 @@ if __name__ == '__main__':
     x_values = df[['x1', 'x2', 'x3']].to_numpy()
     y_values = df['y'].to_numpy()
 
+    k = 5
+    training_testing_pairs = k_cross_validation(k, x_values, y_values)
+    seed:int = 43
+    learning_rates:List[float] = [0.1, 0.05, 0.01]
+    epochs:List[int] = [200]
+
+    second_exercise_training_results_file = open(os.path.join(results_data_dir_name, results_files[1]), "w", newline='')
+    second_exercise_training_results_file.write(f"seed,activation_function,learning_rate,epochs,error\n")
+    second_exercise_results_file = open(os.path.join(results_data_dir_name, "ej2_data_errors.csv"), "w", newline='')
+    second_exercise_results_file.write(f"seed,activation_function,beta,learning_rate,epochs,error_method,training_mean_error,training_error_std,training_data_mean_prediction_error,training_data_prediction_error_std,testing_data_mean_prediction_error,testing_data_prediction_error_std\n")
+
+    # linear perceptron
+    # descendant gradient
+    # using identity
+    training_errors = []
+    training_data_prediction_errors = []
+    testing_data_prediction_errors = []
+    for learning_rate in learning_rates:
+        for epoch_amount in epochs:
+            for configuration in training_testing_pairs:
+                training_set = configuration[0]
+                testing_set = configuration[1]
+
+                perceptron = Perceptron(len(x_values[0]), learning_rate, epoch_amount, identity, prime_identity)
+                breaking_epoch, training_error = perceptron.train(training_set[0], training_set[1], squared_error, 0.9, second_exercise_results_file, "identity", True)
+                training_errors.append(training_error)
+
+                training_data_prediction_error = get_prediction_error(perceptron, training_set[0], training_set[1], squared_error)
+                training_data_prediction_errors.append(training_data_prediction_error)
+
+                testing_data_prediction_error = get_prediction_error(perceptron, testing_set[0], testing_set[1], squared_error)
+                testing_data_prediction_errors.append(testing_data_prediction_error)
+
+            training_mean_error = np.mean(training_errors)
+            training_error_std = np.std(training_errors)
+            training_data_mean_prediction_error = np.mean(training_data_prediction_errors)
+            training_data_prediction_error_std = np.std(training_data_prediction_errors)
+            testing_data_mean_prediction_error = np.mean(testing_data_prediction_errors)
+            testing_data_prediction_error_std = np.std(testing_data_prediction_errors)
+            second_exercise_results_file.write(f"{seed},identity,{learning_rate},{1.0},{breaking_epoch},square_error,{training_mean_error},{training_error_std},{training_data_mean_prediction_error},{training_data_prediction_error_std},{testing_data_mean_prediction_error},{testing_data_prediction_error_std}\n")
+
+    # using tanh(x) with b around [0.01, 0.1] to have a valid aproximation to x
+    beta_values_for_linear = [0.01, 0.05, 0.1]
+
+    training_errors = []
+    training_data_prediction_errors = []
+    testing_data_prediction_errors = []
+    for learning_rate in learning_rates:
+        for epoch_amount in epochs:
+            for beta in beta_values_for_linear:
+                for configuration in training_testing_pairs:
+                    training_set = configuration[0]
+                    testing_set = configuration[1]
+                    perceptron = Perceptron(len(x_values[0]), learning_rate, epoch_amount, tanh, prime_tanh)
+                    breaking_epoch, training_error = perceptron.train(training_set[0], training_set[1], squared_error, 0.9, second_exercise_results_file, f"tanh_linear_b_{beta}", True, beta)
+                    training_errors.append(training_error)
+
+                    training_data_prediction_error = get_prediction_error(perceptron, training_set[0], training_set[1], squared_error)
+                    training_data_prediction_errors.append(training_data_prediction_error)
+
+                    testing_data_prediction_error = get_prediction_error(perceptron, testing_set[0], testing_set[1], squared_error)
+                    testing_data_prediction_errors.append(testing_data_prediction_error)
+
+                training_mean_error = np.mean(training_errors)
+                training_error_std = np.std(training_errors)
+                training_data_mean_prediction_error = np.mean(training_data_prediction_errors)
+                training_data_prediction_error_std = np.std(training_data_prediction_errors)
+                testing_data_mean_prediction_error = np.mean(testing_data_prediction_errors)
+                testing_data_prediction_error_std = np.std(testing_data_prediction_errors)
+                second_exercise_results_file.write(f"{seed},tanh_linear,{beta},{learning_rate},{breaking_epoch},square_error,{training_mean_error},{training_error_std},{training_data_mean_prediction_error},{training_data_prediction_error_std},{testing_data_mean_prediction_error},{testing_data_prediction_error_std}\n")
+
+    #non-linear perceptron
+    beta_values_for_non_linear = [1.0]
+    training_errors = []
+    training_data_prediction_errors = []
+    testing_data_prediction_errors = []
+    for learning_rate in learning_rates:
+        for epoch_amount in epochs:
+            for beta in beta_values_for_linear:
+                for configuration in training_testing_pairs:
+                    training_set = configuration[0]
+                    testing_set = configuration[1]
+                    perceptron = Perceptron(len(x_values[0]), learning_rate, epoch_amount, tanh, prime_tanh)
+                    breaking_epoch, training_error = perceptron.train(training_set[0], training_set[1], squared_error, 0.9, second_exercise_results_file, "tanh_non_linear", True, beta)
+                    training_errors.append(training_error)
+
+                    training_data_prediction_error = get_prediction_error(perceptron, training_set[0], training_set[1], squared_error)
+                    training_data_prediction_errors.append(training_data_prediction_error)
+
+                    testing_data_prediction_error = get_prediction_error(perceptron, testing_set[0], testing_set[1], squared_error)
+                    testing_data_prediction_errors.append(testing_data_prediction_error)
+
+                training_mean_error = np.mean(training_errors)
+                training_error_std = np.std(training_errors)
+                training_data_mean_prediction_error = np.mean(training_data_prediction_errors)
+                training_data_prediction_error_std = np.std(training_data_prediction_errors)
+                testing_data_mean_prediction_error = np.mean(testing_data_prediction_errors)
+                testing_data_prediction_error_std = np.std(testing_data_prediction_errors)
+                second_exercise_results_file.write(f"{seed},tanh_non_linear,{beta},{learning_rate},{breaking_epoch},square_error,{training_mean_error},{training_error_std},{training_data_mean_prediction_error},{training_data_prediction_error_std},{testing_data_mean_prediction_error},{testing_data_prediction_error_std}\n")
+        
+    #for logistic function
+    training_errors = []
+    training_data_prediction_errors = []
+    testing_data_prediction_errors = []
+    for learning_rate in learning_rates:
+        for epoch_amount in epochs:
+            for beta in beta_values_for_linear:
+                for configuration in training_testing_pairs:
+                    training_set = configuration[0]
+                    testing_set = configuration[1]
+                    perceptron = Perceptron(len(x_values[0]), learning_rate, epoch_amount, logistic, prime_logistic)
+                    breaking_epoch, training_error = perceptron.train(training_set[0], training_set[1], squared_error, 0.9, second_exercise_results_file, "logistic", True, beta)
+                    training_errors.append(training_error)
+
+                    training_data_prediction_error = get_prediction_error(perceptron, training_set[0], training_set[1], squared_error)
+                    training_data_prediction_errors.append(training_data_prediction_error)
+
+                    testing_data_prediction_error = get_prediction_error(perceptron, testing_set[0], testing_set[1], squared_error)
+                    testing_data_prediction_errors.append(testing_data_prediction_error)
+
+                training_mean_error = np.mean(training_errors)
+                training_error_std = np.std(training_errors)
+                training_data_mean_prediction_error = np.mean(training_data_prediction_errors)
+                training_data_prediction_error_std = np.std(training_data_prediction_errors)
+                testing_data_mean_prediction_error = np.mean(testing_data_prediction_errors)
+                testing_data_prediction_error_std = np.std(testing_data_prediction_errors)
+                second_exercise_results_file.write(f"{seed},logistic_non_linear,{beta},{learning_rate},{breaking_epoch},square_error,{training_mean_error},{training_error_std},{training_data_mean_prediction_error},{training_data_prediction_error_std},{testing_data_mean_prediction_error},{testing_data_prediction_error_std}\n")
