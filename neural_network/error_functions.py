812e451207683cf384bcfd0ade4dd8a4138d83b9
UrielArias <urielarias.a12@gmail.com>
2025-05-01 03:18:20 -0300

    chore: adding function type in eror_functions module

--- neural_network/error_functions.py
+++ neural_network/error_functions.py
@@ -1,5 +1,6 @@
-from typing import List, Union
+from typing import List, Union, Callable
 import numpy as np
+ErrorFunctionType = Callable[[List[float]], float]
 
 def squared_error(errors:List[float])->Union[int, float]:
     return 0.5 * np.sum(errors ** 2)
