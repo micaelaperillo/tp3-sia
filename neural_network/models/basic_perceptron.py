478b66ac3c38d79561783ae6ba1e8916691fbfc1
UrielArias <urielarias.a12@gmail.com>
2025-05-01 21:33:40 -0300

    refactor: epochs and learning rate passed as parameters of training function

--- neural_network/models/basic_perceptron.py
+++ neural_network/models/basic_perceptron.py
@@ -4,10 +4,8 @@ from neural_network.activation_functions import ActivationFunctionType
 from neural_network.error_functions import ErrorFunctionType
 
 class Perceptron:
-    def __init__(self, input_vector_size:int, learning_rate:float, epochs:int, activation_function:ActivationFunctionType, prime_activation_funcion=None, weights:List[float] = None, seed_value:int = 43):
+    def __init__(self, input_vector_size:int, activation_function:ActivationFunctionType, prime_activation_funcion=None, weights:List[float] = None, seed_value:int = 43):
         self.input_vector_size = input_vector_size
-        self.learning_rate = learning_rate
-        self.epochs = epochs
         self.activation_function = activation_function
         self.prime_activation_function = prime_activation_funcion
         self.seed = seed_value
@@ -22,8 +20,8 @@ class Perceptron:
         h_supra_mu = np.dot(self.weights, input_set) 
         return self.activation_function(h_supra_mu, beta), h_supra_mu
 
-    def train(self, training_set:List[List[int]], labels:List[int], error_function:ErrorFunctionType, max_acceptable_error:float,file, method:str, is_activation_derivable=False, beta:float= 1.0):
-        for epoch in range(self.epochs):
+    def train(self, training_set:List[List[int]], labels:List[int], learning_rate:float, epochs:int, error_function:ErrorFunctionType, max_acceptable_error:float,file, method:str, is_activation_derivable=False, beta:float= 1.0):
+        for epoch in range(epochs):
             errors = []
             for data_instance, label in zip(training_set, labels):
                 data_with_bias = np.insert(data_instance, 0, 1)
@@ -34,15 +32,15 @@ class Perceptron:
                 
                 # we update bias and weights altogether
                 if basic_error != 0:
-                    delta_w = self.learning_rate * basic_error * data_with_bias
+                    delta_w = learning_rate * basic_error * data_with_bias
                     if (is_activation_derivable):
                         delta_w *= self.prime_activation_function(h_supra_mu, beta)
                     self.weights += delta_w
 
             perceptron_error = error_function(np.array(errors))
-            file.write(f"{self.seed},{method},{self.learning_rate},{epoch},{perceptron_error}\n")
+            file.write(f"{self.seed},{method},{learning_rate},{epoch},{perceptron_error}\n")
             if perceptron_error < max_acceptable_error:
                 return epoch + 1, perceptron_error
         
-        return self.epochs, perceptron_error
+        return epochs, perceptron_error
             
