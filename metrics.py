9d58acb91a84953eaffe39a99ab38a86e4cd8437
solrodriguezg <solrodriguez@itba.edu.ar>
2025-05-04 20:14:15 -0300

    metrics: confusion matrix

---
+++ metrics.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+def plot_binary_confusion_matrix(y_true, y_pred, labels=["Negative", "Positive"], title="Binary Confusion Matrix"):
+    cm = confusion_matrix(y_true, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+    disp.plot(cmap=plt.cm.Blues, values_format='d')
+    plt.title(title)
+    plt.show()
+
+
+def plot_multiclass_confusion_matrix(y_true, y_pred, labels=None, title="Multiclass Confusion Matrix"):
+    classes = np.unique(np.concatenate((y_true, y_pred)))
+    if isinstance(labels, dict):
+        # Si es un diccionario: map class -> label
+        labels = [labels[c] for c in classes]
+    else:
+        labels = [f"Class {i}" for i in classes]
+        
+    
+    cm = confusion_matrix(y_true, y_pred, labels=classes)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+    disp.plot(cmap=plt.cm.Oranges, values_format='d')
+    plt.title(title)
+    plt.xticks(rotation=45)
+    plt.show()
+
+
+if __name__ == "__main__":
+    y_true = [0, 1, 1, 0, 1, 0, 1, 1]
+    y_pred = [0, 0, 1, 0, 1, 1, 1, 0]
+    plot_binary_confusion_matrix(y_true, y_pred)
+    plot_multiclass_confusion_matrix(y_true, y_pred, labels={0: "Neg ", 1: "Pos"})
