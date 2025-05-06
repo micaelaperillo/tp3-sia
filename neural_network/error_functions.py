50d177bc5c0db4865ce4be45bceca94dfd9b991d
UrielArias <urielarias.a12@gmail.com>
2025-05-06 19:09:58 -0300

    feat: adding error functions

--- neural_network/error_functions.py
+++ neural_network/error_functions.py
@@ -2,5 +2,8 @@ from typing import List, Union, Callable
 import numpy as np
 ErrorFunctionType = Callable[[List[float]], float]
 
-def squared_error(errors:List[float])->Union[int, float]:
+def squared_error(errors:List[float], y_values:List[float] = None)->Union[int, float]:
     return 0.5 * np.sum(errors ** 2)
+
+def mean_error(errors:List[float], y_values:List[float])->float:
+    return np.abs(np.sum(errors)) / len(y_values)
