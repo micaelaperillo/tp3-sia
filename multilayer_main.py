2f5d40d5bebfe9a564363b9951d9ce4564f9779e
solrodriguezg <solrodriguez@itba.edu.ar>
2025-05-09 18:05:05 -0300

    multilayer main modifications

--- multilayer_main.py
+++ multilayer_main.py
@@ -1,10 +1,19 @@
 import numpy as np
+import os       
+from typing import List
 from neural_network.models.neural_network import NeuralNetwork
 from neural_network.activation_functions import relu, logistic, prime_logistic, relu_derivative
 from neural_network.optimizers import rosenblatt_optimizer, gradient_descent_optimizer
 from neural_network.error_functions import mean_error, squared_error
+from neural_network.partition_methods import k_cross_validation
 
 if __name__ == '__main__':
+
+    results_data_dir_name = "output_data"
+    if not os.path.exists(results_data_dir_name):
+        os.makedirs(results_data_dir_name)
+
+    results_files:List[str] = ["ej3_parity_data.csv", "ej3_digits_data.csv"]
 
     with open("input_data/TP3-ej3-digitos.txt", "r") as file:
         lines = [line.strip() for line in file if line.strip()]
@@ -28,6 +37,14 @@ if __name__ == '__main__':
     learning_rates = [0.0001, 0.05]
     beta_values = [0.01, 0.05, 0.1]
 
+    training_errors = []
+    training_data_prediction_errors = []
+    testing_data_prediction_errors = []
+
+    k = 5
+    training_testing_pairs = k_cross_validation(k, digits_vectors, y_values)
+    parity_results_file = open(os.path.join(results_data_dir_name, results_files[0]), "w", newline='')
+    parity_results_file.write(f"seed,activation_function,neurons_per_layer,beta,learning_rate,epochs,error_method,error\n")
 
     for layer_amount in hidden_layers_amounts:
         for activation_function in activation_functions:
@@ -36,8 +53,19 @@ if __name__ == '__main__':
                     for total_epochs in epochs:
                         for learning_rate in learning_rate:
                             for beta in beta_values:
-                                neural_network = NeuralNetwork(digits_vectors, y_values, layer_amount, activation_function[0], activation_function[1], seed)
-                                training_errors = neural_network.backpropagate(digits_vectors, y_values, learning_rate, total_epochs, optimizer, error_function, beta)
+                                for partition_index, configuration in enumerate(training_testing_pairs):
+
+                                    training_set = configuration[0]
+                                    testing_set = configuration[1]
+
+                                    neural_network = NeuralNetwork(training_set[0], training_set[1], layer_amount, activation_function[0], activation_function[1], seed)
+                                    error = neural_network.backpropagate(training_set[0], training_set[1], learning_rate, total_epochs, optimizer, error_function, beta)
+                                    training_errors.append(error)
+                                    
+                                training_mean_error = np.mean(training_errors)
+                                training_error_std = np.std(training_errors)
+                                parity_results_file.write(f"{seed},{activation_function.__name__},{str(layer_amount)},{beta},{learning_rate},{total_epochs},mean_error,{training_mean_error}\n")
+
 
 
 
@@ -53,6 +81,16 @@ if __name__ == '__main__':
     learning_rates = [0.0001, 0.05]
     beta_values = [0.01, 0.05, 0.1]
 
+    training_errors = []
+    training_data_prediction_errors = []
+    testing_data_prediction_errors = []
+
+    k = 5
+    training_testing_pairs = k_cross_validation(k, digits_vectors, y_values)
+    digits_results_file = open(os.path.join(results_data_dir_name, results_files[1]), "w", newline='')
+    digits_results_file.write(f"seed,activation_function,neurons_per_layer,beta,learning_rate,epochs,error_method,error\n")
+
+
     for layer_amount in hidden_layers_amounts:
         for activation_function in activation_functions:
             for optimizer in optimizers:
@@ -60,6 +98,14 @@ if __name__ == '__main__':
                     for total_epochs in epochs:
                         for learning_rate in learning_rate:
                             for beta in beta_values:
-                                neural_network = NeuralNetwork(digits_vectors, y_values, layer_amount, activation_function[0], activation_function[1], seed)
-                                training_errors = neural_network.backpropagate(digits_vectors, y_values, learning_rate, total_epochs, optimizer, error_function, beta)
+                                for partition_index, configuration in enumerate(training_testing_pairs):
+                                    training_set = configuration[0]
+                                    testing_set = configuration[1]
 
+                                    neural_network = NeuralNetwork(training_set[0], training_set[1], layer_amount, activation_function[0], activation_function[1], seed)
+                                    error = neural_network.backpropagate(training_set[0], training_set[1], learning_rate, total_epochs, optimizer, error_function, beta)
+                                    training_errors.append(error)
+                                    
+                                training_mean_error = np.mean(training_errors)
+                                training_error_std = np.std(training_errors)
+                                digits_results_file.write(f"{seed},{activation_function.__name__},{str(layer_amount)},{beta},{learning_rate},{total_epochs},mean_error,{training_mean_error}\n")
