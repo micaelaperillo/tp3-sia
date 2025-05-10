6919eca62001460d538b978e26066999a51b3de8
tbussozungri <tbussozungri@itba.edu.ar>
2025-05-10 18:54:27 -0300

    fix: changes in basic_perceptron.py

--- basic_perceptron_main.py
+++ basic_perceptron_main.py
@@ -6,11 +6,10 @@ from neural_network.models.basic_perceptron import Perceptron
 from metrics_and_stats.stats import plots_for_exercise_1
 from neural_network.activation_functions import step, identity, prime_identity, tanh, prime_tanh, logistic, prime_logistic
 from neural_network.error_functions import squared_error, mean_error
-from neural_network.partition_methods import k_cross_validation
+from neural_network.partition_methods import k_cross_validation,stratified_k_cross_validation
 from metric_functions import get_prediction_error_for_perceptron
 from neural_network.scale_functions import ScaleFunctions
 from neural_network.optimizers import rosenblatt_optimizer, gradient_descent_optimizer
-
 
 if __name__ == '__main__':
     results_data_dir_name = "output_data"
@@ -29,7 +28,7 @@ if __name__ == '__main__':
 
     seed:int = 43
     learning_rates:List[float] = [0.0001, 0.00005, 0.00001]
-    epochs:List[int] = [200]
+    epochs:List[int] = [1000]
 
     # by storing the seed we get which weights are being used initially
     first_exercise_results_file.write(f"seed,partition,weights,method,beta,learning_rate,total_epochs,epoch,error\n")
@@ -60,13 +59,14 @@ if __name__ == '__main__':
     # Escalo al rango [0, 1] para la función logística y al rango [-1, 1] para la función tanh
     y_values_scaled_logistic = scale_functions.scale_logistic(y_values)
     y_values_scaled_tanh = scale_functions.scale_tanh(y_values)
+    x_values_scaled = scale_functions.scale_logistic(x_values)
 
     k = 5
-    training_testing_pairs_lineal = k_cross_validation(k, x_values, y_values)
+    training_testing_pairs_lineal = k_cross_validation(k, x_values_scaled, y_values_scaled_logistic)
     training_testing_pairs_logistic = k_cross_validation(k, x_values, y_values_scaled_logistic)
     training_testing_pairs_tanh = k_cross_validation(k, x_values, y_values_scaled_tanh)
     seed:int = 43
-    learning_rates:List[float] = [0.0001, 0.00005, 0.00001]
+    learning_rates:List[float] = [1,0.5,0.01,0.0001, 0.00005, 0.00001, 0.000001]
     epochs:List[int] = [200]
 
     second_exercise_training_results_file = open(os.path.join(results_data_dir_name, results_files[1]), "w", newline='')
@@ -87,13 +87,13 @@ if __name__ == '__main__':
                 testing_set = configuration[1]
 
                 perceptron = Perceptron(len(x_values[0]), identity, prime_identity)
-                breaking_epoch, training_error = perceptron.train(training_set[0], training_set[1], learning_rate, epoch_amount,gradient_descent_optimizer,mean_error, 0.1, second_exercise_training_results_file, "identity", True,1.0,partition_index+1)
+                breaking_epoch, training_error = perceptron.train(training_set[0], training_set[1], learning_rate, epoch_amount,gradient_descent_optimizer,mean_error, 0.1, second_exercise_training_results_file, "identity", True,1.0,partition_index+1,descale_fun = scale_functions.descale_logistic)
                 training_errors.append(training_error)
 
-                training_data_prediction_error = get_prediction_error_for_perceptron(perceptron, training_set[0], training_set[1], mean_error)
+                training_data_prediction_error = get_prediction_error_for_perceptron(perceptron, training_set[0], training_set[1], mean_error,1, descale_fun = scale_functions.descale_logistic)
                 training_data_prediction_errors.append(training_data_prediction_error)
 
-                testing_data_prediction_error = get_prediction_error_for_perceptron(perceptron, testing_set[0], testing_set[1], mean_error)
+                testing_data_prediction_error = get_prediction_error_for_perceptron(perceptron, testing_set[0], testing_set[1], mean_error,1, descale_fun = scale_functions.descale_logistic)
                 testing_data_prediction_errors.append(testing_data_prediction_error)
 
             training_mean_error = np.mean(training_errors)
@@ -135,13 +135,13 @@ if __name__ == '__main__':
                 second_exercise_results_file.write(f"{seed},tanh_linear,{beta},{learning_rate},{breaking_epoch},square_error,{training_mean_error},{training_error_std},{training_data_mean_prediction_error},{training_data_prediction_error_std},{testing_data_mean_prediction_error},{testing_data_prediction_error_std}\n")
 
     #non-linear perceptron
-    beta_values_for_non_linear = [1.0]
+    beta_values_for_non_linear = [5,10,70]
     training_errors = []
     training_data_prediction_errors = []
     testing_data_prediction_errors = []
     for learning_rate in learning_rates:
         for epoch_amount in epochs:
-            for beta in beta_values_for_linear:
+            for beta in beta_values_for_non_linear:
                 for partition_index,configuration in enumerate(training_testing_pairs_tanh):
                     training_set = configuration[0]
                     testing_set = configuration[1]
@@ -169,7 +169,7 @@ if __name__ == '__main__':
     testing_data_prediction_errors = []
     for learning_rate in learning_rates:
         for epoch_amount in epochs:
-            for beta in beta_values_for_linear:
+            for beta in beta_values_for_non_linear:
                 for partition_index,configuration in enumerate(training_testing_pairs_logistic):
                     training_set = configuration[0]
                     testing_set = configuration[1]
