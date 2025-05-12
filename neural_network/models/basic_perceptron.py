ee7687558775902723653f8d4d5786b57a89c2e4
tbussozungri <tbussozungri@itba.edu.ar>
2025-05-12 17:06:58 -0300

    feat: add epoch vs error graph for testing

--- neural_network/models/basic_perceptron.py
+++ neural_network/models/basic_perceptron.py
@@ -20,7 +20,7 @@ class Perceptron:
         h_supra_mu = np.dot(self.weights, input_set) 
         return self.activation_function(h_supra_mu, beta), h_supra_mu
 
-    def train(self, training_set:List[List[int]], labels:List[int], learning_rate:float, epochs:int, optimizer, error_function:ErrorFunctionType, max_acceptable_error:float,file, method:str, is_activation_derivable=False, beta:float= 1.0, partition:int=0,descale_fun = None):
+    def train(self, training_set:List[List[int]], labels:List[int], testing_set:List[List[int]], test_labels:List[int], learning_rate:float, epochs:int, optimizer, error_function:ErrorFunctionType, max_acceptable_error:float,file, method:str, is_activation_derivable=False, beta:float= 1.0, partition:int=0,descale_fun = None):
         for epoch in range(epochs):
             for data_instance, label in zip(training_set, labels):
                 data_with_bias = np.insert(data_instance, 0, 1)
@@ -37,6 +37,8 @@ class Perceptron:
                     self.weights += delta_w
 
             errors = []
+            test_errors = []
+            # training error for the current epoch
             for x_value, expected_value in zip(training_set, labels):
                 x_value = np.insert(x_value, 0, 1)
                 prediction, h_supra_mu = self.predict(x_value,beta)
@@ -47,7 +49,23 @@ class Perceptron:
                 errors.append(error)
 
             perceptron_error = error_function(np.array(errors))
-            file.write(f"{self.seed},{partition},{self.weights},{method},{beta},{learning_rate},{epochs},{epoch+1},{perceptron_error}\n")
+
+            if (testing_set is not None) and (test_labels is not None):
+                # testing error for the current epoch
+                for x_value, expected_value in zip(testing_set, test_labels):
+                    x_value = np.insert(x_value, 0, 1)
+                    prediction, h_supra_mu = self.predict(x_value,beta)
+                    if descale_fun is not None:
+                        prediction = descale_fun(prediction)
+                        expected_value = descale_fun(expected_value)
+                    test_error = (expected_value - prediction) 
+                    test_errors.append(test_error)
+
+                perceptron_testing_error = error_function(np.array(test_errors))
+                file.write(f"{self.seed},{partition},{self.weights},{method},{beta},{learning_rate},{epochs},{epoch+1},{perceptron_error},{perceptron_testing_error}\n")
+
+            else:
+                file.write(f"{self.seed},{partition},{self.weights},{method},{beta},{learning_rate},{epochs},{epoch+1},{perceptron_error}\n")
             if perceptron_error < max_acceptable_error:
                 return epoch + 1, perceptron_error
         
